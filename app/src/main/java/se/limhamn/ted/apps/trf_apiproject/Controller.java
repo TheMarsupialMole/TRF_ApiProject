1c93c1b9fa5a9ca4b6b965151755eda8d032caf6
TheMarsupialMole <tmback74@hotmail.com>
2014-10-21 17:03:57 +0200

    Initial commit

---
+++ app/src/main/java/se/limhamn/ted/apps/trf_apiproject/Controller.java
@@ -0,0 +1,14 @@
+package se.limhamn.ted.apps.trf_apiproject;
+
+import android.os.AsyncTask;
+
+/**
+ * Created by Ted on 2014-10-21.
+ */
+public class Controller {
+    private ApiRequestRecipe apiRequestRecipe;
+
+    public Controller(){
+        apiRequestRecipe = new ApiRequestRecipe();
+    }
+}
