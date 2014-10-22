540bd915ebda20be8a7c43b9c462bea7b297b907
Radovan1 <radovan_mojsovski@hotmail.com>
2014-10-22 13:54:02 +0200

    Här kommer första fragmentet

--- app/src/main/java/se/limhamn/ted/apps/trf_apiproject/Controller.java
+++ app/src/main/java/se/limhamn/ted/apps/trf_apiproject/Controller.java
@@ -1,14 +1,21 @@
 package se.limhamn.ted.apps.trf_apiproject;
 
+import android.app.FragmentManager;
+
 /**
  * Created by Ted on 2014-10-21.
  */
 public class Controller {
     private AsyncQueryEngine asyncQueryEngine;
+    private MainActivity main;
+    private SearchFragment frSearch;
 
-    public Controller(){
-
+    public Controller(MainActivity main){
+        this.main = main;
         asyncQueryEngine = new AsyncQueryEngine();
+        FragmentManager fm = main.getFragmentManager();
+        frSearch = (SearchFragment)fm.findFragmentById(R.id.fragSearch);
+        frSearch.setController(this);
 //        asyncQueryEngine.setAndSearchRecipe("lasagna");
     }
 
