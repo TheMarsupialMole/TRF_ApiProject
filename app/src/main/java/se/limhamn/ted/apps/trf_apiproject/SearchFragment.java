75d7e339caa05f4990a98a95e36a7fb1dbb86c65
Radovan1 <radovan_mojsovski@hotmail.com>
2014-10-22 12:42:41 +0200

    Här kommer första fragmentet

--- app/src/main/java/se/limhamn/ted/apps/trf_apiproject/SearchFragment.java
+++ app/src/main/java/se/limhamn/ted/apps/trf_apiproject/SearchFragment.java
@@ -7,7 +7,9 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 
 /**
@@ -15,19 +17,33 @@ import android.view.ViewGroup;
  *
  */
 public class SearchFragment extends Fragment {
-
+    private ListView recipeListView;
+    private Button btnSearch;
+    private EditText etRecipeSearch;
+    private Controller controller;
 
     public SearchFragment() {
         // Required empty public constructor
     }
 
+    public void setController(Controller controller) {
+        this.controller = controller;
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false);
+        View view = inflater.inflate(R.layout.fragment_search, container, false);
+        recipeListView = (ListView)view.findViewById(R.id.lvRecipeList);
+        etRecipeSearch = (EditText)view.findViewById(R.id.etSearch);
+        btnSearch = (Button)view.findViewById(R.id.btnSearchRecipe);
+        btnSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                controller.searchRecipe(etRecipeSearch.getText().toString());
+            }
+        });
+        return view;
     }
-
-
 }
